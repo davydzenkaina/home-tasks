3d784930610216cd5182056884e4bbe330ce55f6
Ina Davydzenka <davydzenkaina@gmail.com>
2022-08-16 17:29:23 +0300

    Delete

--- learning.js
+++
@@ -1,2 +0,0 @@
-const text = 'Hello NodeJS'
-export default text 
--- stage2.js
+++
@@ -1,9 +0,0 @@
-
-//Объявить и присвоить значение переменной в одном файле
-
- //и вывести в консоль эту переменную в другом файле
-
-import chalk from 'chalk'; 
-import text from './learning.js';
-console.log(chalk.green(text)); 
-
